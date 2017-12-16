9448b2e1c85d040f9fae2e94183bb712c01e9852
Cole <coleaaronmueller@gmail.com>
2017-12-16 11:32:38 -0500

    Added wall scaling, trying to add android support

---
+++ Drippy/Assets/_scripts/ScaleWidth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleWidth : MonoBehaviour {
+
+    
+    //[ExecuteInEditMode]
+    void Start()
+    {
+        float height = Camera.main.orthographicSize * 2;
+        float width = height * Screen.width / Screen.height; // basically height * screen aspect ratio
+
+        this.transform.localScale = new Vector3(width/6.5f, height/10, 0);
+    }
+}
