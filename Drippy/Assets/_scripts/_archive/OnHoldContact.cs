8c68be5010f48b2568997f376a96f0301fbc38a4
Cole <coleaaronmueller@gmail.com>
2020-02-02 11:20:14 -0500

    fuk

--- Drippy/Assets/_scripts/_archive/OnHoldContact.cs
+++ Drippy/Assets/_scripts/_archive/OnHoldContact.cs
@@ -10,10 +10,13 @@ public class OnHoldContact : MonoBehaviour {
     private AudioSource drop;
     private Rigidbody2D playerRigidbody;
     private GameObject currentHold;
+    private GameObject currentPlatform;
     //public Camera cam;
     private bool doSway = false;
     public float swaySpeed = 5;
     public float swayDistance = 60;
+    public float x_force_mult = 3.5f;
+    public float y_force_mult = .5f;
     private float swing_mult;
     private float prev_angle;
     private float current_angle;
@@ -69,6 +72,28 @@ public class OnHoldContact : MonoBehaviour {
             drop.Play();
             canTap = true;
         }
+        else if (collision.CompareTag("platform_edge"))
+        {
+            currentPlatform.GetComponent<BoxCollider2D>().enabled = false;
+            // put the transition back to falling sprite here
+            transform.localScale = new Vector3(100, 100, 100);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "platform")
+        {
+            currentPlatform = collision.gameObject;
+            BoxCollider2D[] side_cols = currentPlatform.GetComponentsInChildren<BoxCollider2D>();
+            foreach (BoxCollider2D bc in side_cols) {
+                bc.enabled = true;
+            }
+            // put the transition to slide sprite here
+            transform.localScale = new Vector3(100, 50, 100);
+
+            Debug.Log("platform");
+        }
     }
 
     public void OnScreenTap()
@@ -83,8 +108,8 @@ public class OnHoldContact : MonoBehaviour {
             doSway = false;
             playerRigidbody.gravityScale = 1;
             // 
-            playerRigidbody.velocity = new Vector2(Mathf.Cos(Mathf.Deg2Rad*(transform.localEulerAngles.z-90f))*2f*swing_mult,
-                                                   Mathf.Abs(Mathf.Cos(Mathf.Deg2Rad*(transform.localEulerAngles.z-90f)))*2f);
+            playerRigidbody.velocity = new Vector2(Mathf.Cos(Mathf.Deg2Rad*(transform.localEulerAngles.z-90f))*x_force_mult*swing_mult,
+                                                   (Mathf.Abs(Mathf.Cos(Mathf.Deg2Rad*(transform.localEulerAngles.z-90f)))+y_force_mult)*2f*swing_mult);
             canTap = false;
         }
     }
