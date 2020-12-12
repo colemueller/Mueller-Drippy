03e3113c2b9a4c75e6ff4526573b58fcc5749b88
Unknown <adb96@drexel.edu>
2020-12-12 13:30:11 -0700

    Starting trying to work on platform rotation

---
+++ Drippy/Assets/_scripts/grabRotate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabRotate : MonoBehaviour
+{
+
+    private Transform platform;
+    private Touch the_touch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        platform = this.GetComponentInParent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Debug.Log(Input.touchCount);
+            the_touch = Input.GetTouch(0);
+
+            if (the_touch.phase == TouchPhase.Began)
+            {
+                if (the_touch.position.x <= this.GetComponent<BoxCollider2D>().bounds.max.x && the_touch.position.y <= this.GetComponent<BoxCollider2D>().bounds.max.y &&
+                    the_touch.position.x >= this.GetComponent<BoxCollider2D>().bounds.min.x && the_touch.position.y >= this.GetComponent<BoxCollider2D>().bounds.min.y)
+                {
+                    Debug.Log("Touch");
+                }
+            }
+        }
+    }
+}
